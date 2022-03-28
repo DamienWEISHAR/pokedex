706709de937207aa66dee5d76de2c12f5f168037
DamienWEISHAR <88081429+DamienWEISHAR@users.noreply.github.com>
2022-03-28 21:13:17 +0200

    début

---
+++ src/w/damien/pokedex/Main.java
@@ -0,0 +1,9 @@
+package w.damien.pokedex;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pokedex pkmn = new Pokedex();
+        System.out.println(pkmn); //imprime le package+nom+adresse de l'objet
+    }
+}
---
+++ src/w/damien/pokedex/Pokedex.java
@@ -0,0 +1,17 @@
+package w.damien.pokedex;
+
+public class Pokedex {
+
+    //ATTRIBUTS
+    private String nom;
+    private int numero;
+    private String type;
+    private double taille;
+    private double poids;
+    private String description;
+
+    //CONSTRUCTEUR
+    public Pokedex(){
+        System.out.println("Constructeur de Pokemon Vide");
+    }
+}
